d7002a73fa348360fb71dd2279ee4e1548110f1d
GabJeungSook <gicawalo@gmail.com>
2023-03-15 20:26:13 +0800

    can add basic information

--- app/Http/Livewire/Admin/Masterlist.php
+++ app/Http/Livewire/Admin/Masterlist.php
@@ -9,11 +9,42 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use App\Models\BasicInformation;
 use Filament\Tables\Columns\TextColumn;
+use WireUi\Traits\Actions;
+use DB;
 
 class Masterlist extends Component implements Tables\Contracts\HasTable
 {
     public $add_modal = false;
+    public $_number;
+    public $_lot_number;
+    public $_survey_number;
+    public $_title_area;
+    public $_awarded_area;
+    public $_previous_land_owner;
+    public $_field_number;
+    public $_location;
+    public $_municipality;
+    public $_title;
+    public $_cloa_number;
+    public $_page;
+    public $_encumbered_area;
+    public $_encumbered_variance;
+    public $_previous_copy_of_title_type_of_title;
+    public $_previous_copy_of_title_number;
+    public $_title_status;
+    public $_title_copy;
+    public $_remarks;
+    public $_status;
+    public $_land_bank_amortization;
+    public $_amount;
+    public $_date_paid;
+    public $_date_of_cert;
+    public $_ndc_direct_payment_scheme;
+    public $_ndc_remarks;
+    public $_notes;
+
     use Tables\Concerns\InteractsWithTable;
+    use Actions;
 
     protected function getTableQuery(): Builder
     {
@@ -118,6 +149,55 @@ class Masterlist extends Component implements Tables\Contracts\HasTable
                 ->searchable()
                 ->sortable(),
         ];
+    }
+
+
+    public function saveBasicInformation()
+    {
+
+        $encumbered = json_encode([
+            'area' => $this->_encumbered_area,
+            'variance' => $this->_encumbered_variance,
+        ]);
+        $previous_copy_of_title = json_encode([
+            'type of title' => $this->_previous_copy_of_title_type_of_title,
+            'no.' => $this->_previous_copy_of_title_number,
+        ]);
+        DB::beginTransaction();
+        BasicInformation::create([
+            'number' => $this->_number,
+            'lot_number' => $this->_lot_number,
+            'survey_number' => $this->_survey_number,
+            'title_area' => $this->_title_area,
+            'awarded_area' => $this->_awarded_area,
+            'previous_land_owner' => $this->_previous_land_owner,
+            'field_number' => $this->_field_number,
+            'location' => $this->_location,
+            'municipality' => $this->_municipality,
+            'title' => $this->_title,
+            'cloa_number' => $this->_cloa_number,
+            'page' => $this->_page,
+            'encumbered' => $encumbered,
+            'previous_copy_of_title' => $previous_copy_of_title,
+            'title_status' => $this->_title_status,
+            'title_copy' => $this->_title_copy,
+            'remarks' => $this->_remarks,
+            'status' => $this->_status,
+            'land_bank_amortization' => $this->_land_bank_amortization,
+            'amount' => $this->_amount,
+            'date_paid' => \Carbon\Carbon::parse($this->_date_paid)->format('Y-m-d'),
+            'date_of_cert' => \Carbon\Carbon::parse($this->_date_of_cert)->format('Y-m-d'),
+            'ndc_direct_payment_scheme' => $this->_ndc_direct_payment_scheme,
+            'ndc_remarks' => $this->_ndc_remarks,
+            'notes' => $this->_notes,
+         ]);
+         DB::commit();
+         $this->add_modal = false;
+
+         $this->dialog()->success(
+            $title = 'Success',
+            $description = 'Data successfully saved'
+        );
     }
 
     public function render()
--- resources/views/livewire/admin/masterlist.blade.php
+++ resources/views/livewire/admin/masterlist.blade.php
@@ -8,54 +8,63 @@
   <x-modal wire:model.defer="add_modal" align="center" max-width="6xl">
     <x-card title="NEW LAND OWNER">
       <div class="grid grid-cols-5 gap-4">
-        <x-input wire:model="firstName" label="No." placeholder="" />
-        <x-input wire:model="firstName" label="Lot No." placeholder="" />
-        <x-input wire:model="firstName" label="Survey No." placeholder="" />
-        <x-input wire:model="Title Area Has.S" label="Title Area Has.S" placeholder="" />
-        <x-input wire:model="Title Area Has.S" label="Awarded Area Has.S" placeholder="" />
-        <x-input wire:model="Title Area Has.S" label="Previous Land Owner" placeholder="" />
-        <x-input wire:model="Title Area Has.S" label="Field No." placeholder="" />
-        <x-input wire:model="Title Area Has.S" label="Location" placeholder="" />
-        <x-input wire:model="Title Area Has.S" label="Municipality" placeholder="" />
-        <x-input wire:model="Title Area Has.S" label="Title" placeholder="" />
-        <x-input wire:model="Title Area Has.S" label="Cloa No." placeholder="" />
-        <x-input wire:model="Title Area Has.S" label="Page" placeholder="" />
+        <x-input wire:model="_number" label="No." placeholder="" />
+        <x-input wire:model="_lot_number" label="Lot No." placeholder="" />
+        <x-input wire:model="_survey_number" label="Survey No." placeholder="" />
+        <x-input wire:model="_title_area" label="Title Area Has.S" placeholder="" />
+        <x-input wire:model="_awarded_area" label="Awarded Area Has.S" placeholder="" />
+        <x-input wire:model="_previous_land_owner" label="Previous Land Owner" placeholder="" />
+        <x-input wire:model="_field_number" label="Field No." placeholder="" />
+        <x-input wire:model="_location" label="Location" placeholder="" />
+        <x-input wire:model="_municipality" label="Municipality" placeholder="" />
+        <x-input wire:model="_title" label="Title" placeholder="" />
+        <x-input wire:model="_cloa_number" label="Cloa No." placeholder="" />
+        <x-input wire:model="_page" label="Page" placeholder="" />
         <div class="col-span-5 border-y-2 py-3 border-gray-500">
-          <h1>Encumbered</h1>
-          <div class="grid grid-cols-4 gap-4 mt-1">
-            <x-input wire:model="Title Area Has.S" label="Area" placeholder="" />
-            <x-input wire:model="Title Area Has.S" label="Variance" placeholder="" />
-            <x-input wire:model="Title Area Has.S" label="Previous copy of title type" placeholder="" />
-            <x-input wire:model="Title Area Has.S" label="Previous copy of title no." placeholder="" />
+            <div class="grid grid-cols-2">
+                <h1>Encumbered</h1>
+                <h1>Previous Copy Of Title</h1>
+            </div>
+          <div class="grid grid-cols-2 gap-4 mt-1">
+            <div class="grid grid-cols-2 gap-4 mt-1">
+            <x-input wire:model="_encumbered_area" label="Area" placeholder="" />
+            <x-input wire:model="_encumbered_variance" label="Variance" placeholder="" />
+            </div>
+            <div class="grid grid-cols-2 gap-4 mt-1">
+            <x-input wire:model="_previous_copy_of_title_type_of_title" label="Type Of Title" placeholder="" />
+            <x-input wire:model="_previous_copy_of_title_number" label="No." placeholder="" />
+            </div>
           </div>
         </div>
-        <x-input wire:model="Title Area Has.S" label="Title Status" placeholder="" />
-        <x-input wire:model="Title Area Has.S" label="Title Copy" placeholder="" />
-        <x-input wire:model="Title Area Has.S" label="Title Status" placeholder="" />
-        <x-input wire:model="Title Area Has.S" label="Land Bank Amortization" placeholder="" />
+        {{-- <x-input wire:model="_title_status" label="Title Status" placeholder="" /> --}}
+        <x-input wire:model="_title_copy" label="Title Copy" placeholder="" />
+        <x-input wire:model="_title_status" label="Title Status" placeholder="" />
+        <x-input wire:model="_land_bank_amortization" label="Land Bank Amortization" placeholder="" />
         <div class="col-span-3">
-          <x-textarea wire:model="comment" label="Remarks" placeholder="Your comment" />
+          <x-textarea wire:model="_remarks" label="Remarks" placeholder="Your comment" />
         </div>
         <div class="col-span-2">
-          <x-textarea wire:model="comment" label="Status" placeholder="Your comment" />
+          <x-textarea wire:model="_status" label="Status" placeholder="Your comment" />
         </div>
 
-        <x-input wire:model="Title Area Has.S" label="Amount" placeholder="" />
-        <x-input wire:model="Title Area Has.S" label="Date Paid" placeholder="" />
-        <x-input wire:model="Title Area Has.S" label="Date of Certificate" placeholder="" />
-        <x-input wire:model="Title Area Has.S" label="NDC Direct Payment Scheme" placeholder="" />
+        <x-input wire:model="_amount" label="Amount" placeholder="" />
+        {{-- <x-input wire:model="" label="Date Paid" placeholder="" /> --}}
+        <x-datetime-picker without-time label="Date Paid" placeholder="Date Paid" wire:model.defer="_date_paid"/>
+        <x-datetime-picker without-time label="Date of Certificate" placeholder="Date of Certificate" wire:model.defer="_date_of_cert"/>
+        {{-- <x-input wire:model="" label="Date of Certificate" placeholder="" /> --}}
+        <x-input wire:model="_ndc_direct_payment_scheme" label="NDC Direct Payment Scheme" placeholder="" />
         <div class="col-span-3">
-          <x-textarea wire:model="comment" label="NDC Remarks" placeholder="Your comment" />
+          <x-textarea wire:model="_ndc_remarks" label="NDC Remarks" placeholder="Your comment" />
         </div>
         <div class="col-span-2">
-          <x-textarea wire:model="comment" label="Notes" placeholder="Your comment" />
+          <x-textarea wire:model="_notes" label="Notes" placeholder="Your comment" />
         </div>
       </div>
 
       <x-slot name="footer">
         <div class="flex justify-end gap-x-4">
           <x-button flat label="Cancel" x-on:click="close" />
-          <x-button indigo right-icon="save-as" label="Save Owner" />
+          <x-button indigo right-icon="save-as" label="Save Owner" wire:click="saveBasicInformation" />
         </div>
       </x-slot>
     </x-card>
