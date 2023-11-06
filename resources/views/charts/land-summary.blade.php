3c237e65a0dc0b66a357c7d6b9bebcede83ab4b2
GabJeungSook <gicawalo@gmail.com>
2023-11-06 16:32:18 +0800

    temporary disabled onclick on charts

--- resources/views/charts/land-summary.blade.php
+++ resources/views/charts/land-summary.blade.php
@@ -39,15 +39,15 @@
         },
         options: {
           responsive: true,
-          onClick: (event, elements) => {
-            if (elements.length > 0) {
-              const clickedDatasetIndex = elements[0].datasetIndex;
-              const clickedIndex = elements[0].index;
-              const clickedValue = landSummaryChart.data.datasets[clickedDatasetIndex].data[clickedIndex];
-              const clickedLabel = landSummaryChart.data.labels[clickedIndex];
-              Livewire.emit('showReport', clickedLabel, clickedValue);
-            }
-          }
+        //   onClick: (event, elements) => {
+        //     if (elements.length > 0) {
+        //       const clickedDatasetIndex = elements[0].datasetIndex;
+        //       const clickedIndex = elements[0].index;
+        //       const clickedValue = landSummaryChart.data.datasets[clickedDatasetIndex].data[clickedIndex];
+        //       const clickedLabel = landSummaryChart.data.labels[clickedIndex];
+        //       Livewire.emit('showReport', clickedLabel, clickedValue);
+        //     }
+        //   }
         }
       });
     }
